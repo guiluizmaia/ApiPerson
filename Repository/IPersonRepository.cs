b229451315734d9b753d51cf563749aeefb4c74f
guiluizmaia <68974994+guiluizmaia@users.noreply.github.com>
2021-03-06 16:41:52 -0300

    Add files via upload

---
+++ Repository/IPersonRepository.cs
@@ -0,0 +1,16 @@
+﻿
+using IntegraçãoBD.Model;
+using System.Collections.Generic;
+
+namespace IntegraçãoBD.Repository
+{
+    public interface IPersonRepository
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+        bool Exists(long id);
+    }
+}
