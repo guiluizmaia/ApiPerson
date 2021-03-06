ad73561d6f7c96732db21de963e9fb645a66caf2
guiluizmaia <68974994+guiluizmaia@users.noreply.github.com>
2021-03-05 22:03:13 -0300

    Usando outros metodos http

---
+++ Services/IPersonService.cs
@@ -0,0 +1,15 @@
+﻿using CalculadoraCurso.Model;
+using System.Collections.Generic;
+
+namespace CalculadoraCurso.Services.Implementations
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+
+    }
+}
