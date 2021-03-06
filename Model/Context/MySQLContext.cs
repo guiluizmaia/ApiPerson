df2f0e97cb510001a33ce484651723a308135094
guiluizmaia <68974994+guiluizmaia@users.noreply.github.com>
2021-03-06 12:04:46 -0300

    Integração com o BD concluida

---
+++ Model/Context/MySQLContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IntegraçãoBD.Model.Context
+{
+    public class MySQLContext : DbContext 
+    {
+        public MySQLContext()
+        {
+
+        }
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options){}
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
