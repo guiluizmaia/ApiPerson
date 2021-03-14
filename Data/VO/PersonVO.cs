49b22d319aca45bacb7c1b3ee476727a2387d9ee
guiluizmaia <68974994+guiluizmaia@users.noreply.github.com>
2021-03-14 00:04:19 -0300

    mudando json

--- Data/VO/PersonVO.cs
+++ Data/VO/PersonVO.cs
@@ -1,11 +1,22 @@
-﻿namespace IntegraçãoBD.Data.VO
+﻿using System.Text.Json.Serialization;
+
+namespace IntegraçãoBD.Data.VO
 {
     public class PersonVO
-    {        
+    {     
+        [JsonPropertyName("code")]
         public long Id { get; set; }
+
+        [JsonPropertyName("name")]
         public string FirstName { get; set; }
+        
+        [JsonPropertyName("last_name")]
         public string LastName { get; set; }
+        
+        [JsonIgnore]
         public string Address { get; set; }
+
+        [JsonPropertyName("sex")]
         public string Gender { get; set; }
     }
 }
